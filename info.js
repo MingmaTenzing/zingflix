515bb5c8ec11a28cbc941bbc8ad02730bb74c433
MingmaTenzing <pasangsherpa689@gmail.com>
2022-11-13 07:23:08 +0800

    info.html is now working with data of movies is showing except genre now need to do the the final designing and mobile compatibility and thats it

--- info.js
+++ info.js
@@ -2,45 +2,50 @@
 const movieId = localStorage.getItem('id')
 
 async function showMovieInfo() {
+  
     const data = await fetch (`http://www.omdbapi.com/?i=${movieId}&apikey=19d18a42`);
     const moviedetails = await data.json();
-    console.log(moviedetails)
+    console.log(moviedetails.Genre)
+   
+   const infoEl = document.querySelector('.movie__container');
+   infoEl.innerHTML = renderHTML(moviedetails)
 }
 
 showMovieInfo()
 
 
 
-function renderHTML (detail) {
+function renderHTML(moviedetails) {
     return `  
     <div class="image__placeholder">
       <figure class="result__img">
         <img
-          src="${detail.Poster}"
+          src="${moviedetails.Poster}"
           class="result__image"
         />
       </figure>
     </div>
 
     <div class="movie__info--section">
-      <h1 class="movie__title padding">${detail.Title}</h1>
+      <h1 class="movie__title padding">${moviedetails.Title}</h1>
       <h4 class="movie__released padding">
-        Release Date: <span>${detail.Released}</span>
+        Release Date: <span>${moviedetails.Released}</span>
       </h4>
       
-      <h4 class="movie__imdbrating padding">IMDb Rating: <span class="data">${detail.imdbRating}</span></h4>
-      <h4 class="movie__imdbvotes padding">IMDb Votes: <span class="data"> ${detail.imdbVotes}</span></h4>
-      <h4 class="movie__metascore padding">Metascore: <span class="data">${detail.Metascore}</span></h4>
-      <h4 class="movie__boxoffice padding">BoxOffice: <span class="data">${detail.BoxOffice}</span></h4>
-      <h4 class="movie__genre padding">Genre: <span class="data"><${detail.Genre}></span></h4>
-      <h4 class="movie__director padding">Director: <span class="data">${detail.Director}</span></h4>
-      <h4 class="movie__writer padding">Writer: <span class="data">${detail.Writer}</span></h4>
-      <h4 class="movie__actors padding">Actors: <span class="data"> ${detail.Actors}</span></h4>
+      <h4 class="movie__imdbrating padding">IMDb Rating: <span class="data">${moviedetails.imdbRating}</span></h4>
+      <h4 class="movie__imdbvotes padding">IMDb Votes: <span class="data"> ${moviedetails.imdbVotes}</span></h4>
+      <h4 class="movie__metascore padding">Metascore: <span class="data">${moviedetails.Metascore}</span></h4>
+      <h4 class="movie__boxoffice padding">BoxOffice: <span class="data">${moviedetails.BoxOffice}</span></h4>
+      
+      <h4 class="movie__genre padding">Genre: <span class="data"><${moviedetails.Genre}></span></h4>
+      <h4 class="movie__director padding">Director: <span class="data">${moviedetails.Director}</span></h4>
+      <h4 class="movie__writer padding">Writer: <span class="data">${moviedetails.Writer}</span></h4>
+      <h4 class="movie__actors padding">Actors: <span class="data"> ${moviedetails.Actors}</span></h4>
       <p class="movie__plot padding">
-        Plot: <span class="data">${detail.Plot}</span>
+        Plot: <span class="data">${moviedetails.Plot}</span>
       </p>
-      <h4 class="movie__language padding">Language: <span class="data">${detail.Language}</span></h4>
-      <h4 class="movie__awards padding">Awards: <span class="data"> ${detail.Awards}</span> </h4>
+      <h4 class="movie__language padding">Language: <span class="data">${moviedetails.Language}</span></h4>
+      <h4 class="movie__awards padding">Awards: <span class="data"> ${moviedetails.Awards}</span> </h4>
 
 
 
